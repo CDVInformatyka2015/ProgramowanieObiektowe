6d6462ee93d7492f1905d4ec469c68bd7691e2e9
potlewsk <potlewsk@CDV-N10504>
2016-11-12 10:08:17 +0100

    Kolekcja

--- Interfejsy_12.11.2016/Program.cs
+++ Interfejsy_12.11.2016/Program.cs
@@ -1,10 +1,45 @@
 ﻿using System;
+using System.Collections;
 
 namespace Interfejsy_12._11._2016
 {
     class Program
     {
         static void Main(string[] args)
+        {
+            //runBigos();
+            dawajBigos();
+            Console.ReadKey();
+        }
+
+        private static void dawajBigos()
+        {
+            ArrayList lista = new ArrayList();
+            bool status = true;
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("Każdy składnik zatwierdzasz Enterem, jeśli chcesz zakończyć dodawanie składników - wpisz 'koniec'");
+                Console.Write("Jaki składnik: ");
+                string skladnik = Console.ReadLine();
+                if (skladnik == "koniec")
+                {
+                    status = false;
+                    break;
+                }
+                lista.Add(skladnik);
+            } while (status);
+
+            IEnumerator e = lista.GetEnumerator();
+            Console.Clear();
+            Console.WriteLine("A oto i lista składników!");
+            while (e.MoveNext())
+            {
+                Console.WriteLine(">" + e.Current);
+            }
+        }
+
+        private static void runBigos()
         {
             Bigos bigos = new Bigos(true);
             Console.Clear();
